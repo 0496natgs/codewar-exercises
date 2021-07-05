3437556f38bb8c2278c946acb13f7a458390a719
NatusHuitzi <natalia@huitzisolutions.io>
2021-07-05 14:20:48 -0500

    separar cada paso en una línea distinta

--- reverse-words.js
+++ reverse-words.js
@@ -2,7 +2,12 @@
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
 function reverseWords(str) {
-  return str.split('').reverse().join('').split(' ').reverse().join(' ');
+  return str.split('')
+  .reverse()
+  .join('')
+  .split(' ')
+  .reverse()
+  .join(' ');
 }
 
 console.log(reverseWords("This is an example!"));
